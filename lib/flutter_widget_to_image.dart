42073ae68d390f1a1222ee5a9808ba2d4d15c099
Minh Dũng Lê <leminhdungltv@gmail.com>
2025-03-05 23:44:02 +0700

    upgrade flutter 3.27.x

--- lib/flutter_widget_to_image.dart
+++ lib/flutter_widget_to_image.dart
@@ -15,188 +15,55 @@ class FlutterWidgetToImage {
     BuildContext? context,
     required double widthImage,
   }) async {
-    //
-    ///Retry counter
-    ///
-    int retryCounter = 3;
-    bool isDirty = false;
-
-    Widget child = widget;
-
-    if (context != null) {
-      ///
-      ///Inherit Theme and MediaQuery of app
-      ///
-      ///
-      child = InheritedTheme.captureAll(
-        context,
-        MediaQuery(
-            data: MediaQuery.of(context),
-            child: Material(
-              child: child,
-              color: Colors.transparent,
-            )),
-      );
-    }
-
-    final RenderRepaintBoundary repaintBoundary = RenderRepaintBoundary();
-    final platformDispatcher = WidgetsBinding.instance.platformDispatcher;
-    final fallBackView = platformDispatcher.views.first;
-    final view = context == null ? fallBackView : View.maybeOf(context) ?? fallBackView;
-    Size logicalSize = view.physicalSize / view.devicePixelRatio; // Adapted
-    Size imageSize = view.physicalSize; // Adapted
-
-    assert(logicalSize.aspectRatio.toStringAsPrecision(5) ==
-        imageSize.aspectRatio.toStringAsPrecision(5)); // Adapted (toPrecision was not available)
-
-    final RenderView renderView = RenderView(
-      view: view,
-      child: RenderPositionedBox(alignment: Alignment.center, child: repaintBoundary),
-      configuration: ViewConfiguration(
-        // size: logicalSize,
-        logicalConstraints: BoxConstraints(
-          maxWidth: logicalSize.width,
-          maxHeight: logicalSize.height,
-        ),
-        devicePixelRatio: pixelRatio ?? 1.0,
-      ),
-    );
-
-    final PipelineOwner pipelineOwner = PipelineOwner();
-    final BuildOwner buildOwner = BuildOwner(
-        focusManager: FocusManager(),
-        onBuildScheduled: () {
-          ///
-          ///current render is dirty, mark it.
-          ///
-          isDirty = true;
-        });
-
-    pipelineOwner.rootNode = renderView;
-    renderView.prepareInitialFrame();
-
-    final RenderObjectToWidgetElement<RenderBox> rootElement = RenderObjectToWidgetAdapter<RenderBox>(
-        container: repaintBoundary,
-        child: Directionality(
-          textDirection: TextDirection.ltr,
-          child: child,
-        )).attachToRenderTree(
-      buildOwner,
-    );
-    ////
-    ///Render Widget
-    ///
-    ///
-
-    buildOwner.buildScope(
-      rootElement,
-    );
-    buildOwner.finalizeTree();
-
-    pipelineOwner.flushLayout();
-    pipelineOwner.flushCompositingBits();
-    pipelineOwner.flushPaint();
-
-    ui.Image? image;
-
-    do {
-      ///
-      ///Reset the dirty flag
-      ///
-      ///
-      isDirty = false;
-
-      image = await repaintBoundary.toImage(pixelRatio: pixelRatio ?? (imageSize.width / logicalSize.width));
-
-      ///
-      ///This delay sholud increas with Widget tree Size
-      ///
-
-      await Future.delayed(delay);
-
-      ///
-      ///Check does this require rebuild
-      ///
-      ///
-      if (isDirty) {
-        ///
-        ///Previous capture has been updated, re-render again.
-        ///
-        ///
-        buildOwner.buildScope(
-          rootElement,
-        );
-        buildOwner.finalizeTree();
-        pipelineOwner.flushLayout();
-        pipelineOwner.flushCompositingBits();
-        pipelineOwner.flushPaint();
-      }
-      retryCounter--;
-
-      ///
-      ///retry untill capture is successfull
-      ///
-    } while (isDirty && retryCounter >= 0);
     try {
-      /// Dispose All widgets
-      // rootElement.visitChildren((Element element) {
-      //   rootElement.deactivateChild(element);
-      // });
-      buildOwner.finalizeTree();
-    } catch (e) {}
+      // 1. Tạo RepaintBoundary để render widget
+      final RenderRepaintBoundary boundary = RenderRepaintBoundary();
 
-    ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    return byteData?.buffer.asUint8List();
-    // WidgetsFlutterBinding.ensureInitialized();
-    // final RenderRepaintBoundary repaintBoundary = RenderRepaintBoundary();
-    //
-    // final PipelineOwner pipelineOwner = PipelineOwner();
-    // final BuildOwner buildOwner = BuildOwner(focusManager: FocusManager());
-    // try {
-    //   final RenderView renderView = RenderView(
-    //     window: ui.window,
-    //     child: RenderPositionedBox(alignment: Alignment.center, child: repaintBoundary),
-    //     configuration: ViewConfiguration(
-    //       size: Size(widthImage, 1000),
-    //       devicePixelRatio: pixelRatio,
-    //     ),
-    //   );
-    //
-    //   pipelineOwner.rootNode = renderView;
-    //   renderView.prepareInitialFrame();
-    //   final MediaQueryData query = MediaQueryData.fromWindow(ui.window);
-    //   final RenderObjectToWidgetElement<RenderBox> rootElement = RenderObjectToWidgetAdapter<RenderBox>(
-    //     container: repaintBoundary,
-    //     child: MediaQuery(
-    //       data: query.copyWith(
-    //         devicePixelRatio: pixelRatio,
-    //         textScaleFactor: 1,
-    //         boldText: false,
-    //       ),
-    //       child: Directionality(
-    //         textDirection: TextDirection.ltr,
-    //         child: widget,
-    //       ),
-    //     ),
-    //   ).attachToRenderTree(buildOwner);
-    //   buildOwner.buildScope(rootElement);
-    //   buildOwner.finalizeTree();
-    //   pipelineOwner.flushLayout();
-    //   pipelineOwner.flushCompositingBits();
-    //   pipelineOwner.flushPaint();
-    //   final ui.Image image = await repaintBoundary.toImage(pixelRatio: pixelRatio);
-    //
-    //   final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
-    //
-    //   var buff = byteData!.buffer.asUint8List();
-    //
-    //   var decodedImage = await decodeImageFromList(buff);
-    //   print(decodedImage.width);
-    //   print(decodedImage.height);
-    //   return buff;
-    // } catch (e) {
-    //   print("___ $e");
-    // }
-    // return null;
+      // 2. Tạo một RenderView mới trong bộ nhớ
+      final PipelineOwner pipelineOwner = PipelineOwner();
+      final BuildOwner buildOwner = BuildOwner(focusManager: FocusManager());
+      final RenderView renderView = RenderView(
+        child: RenderPositionedBox(alignment: Alignment.center, child: boundary),
+        configuration: ViewConfiguration(
+            logicalConstraints: BoxConstraints(
+              maxWidth: widthImage,
+              maxHeight: 10000,
+            ),
+            devicePixelRatio: pixelRatio),
+        view: WidgetsBinding.instance.platformDispatcher.views.first, // Cập nhật cho Flutter 3.27.x
+      );
+
+      pipelineOwner.rootNode = renderView;
+      renderView.prepareInitialFrame();
+
+      final MediaQueryData query = MediaQueryData.fromView(WidgetsBinding.instance.platformDispatcher.views.first);
+      // 3. Attach widget vào RenderObject
+      final RenderObjectToWidgetElement<RenderBox> element = RenderObjectToWidgetAdapter<RenderBox>(
+        container: boundary,
+        child: MediaQuery(
+          data: query.copyWith(
+            devicePixelRatio: pixelRatio,
+            textScaler: TextScaler.linear(1),
+            boldText: false,
+          ),
+          child: Directionality(
+            textDirection: TextDirection.ltr,
+            child: widget,
+          ),
+        ),
+      ).attachToRenderTree(buildOwner);
+
+      // 4. Xây dựng và vẽ widget
+      buildOwner.buildScope(element);
+      pipelineOwner.flushLayout();
+      pipelineOwner.flushCompositingBits();
+      pipelineOwner.flushPaint();
+
+      // 5. Chuyển thành ảnh Uint8List
+      final ui.Image image = await boundary.toImage(pixelRatio: pixelRatio);
+      final ByteData? byteData = await image.toByteData(format: ui.ImageByteFormat.png);
+      return byteData?.buffer.asUint8List();
+    } catch (ex) {}
+    return null;
   }
 }
